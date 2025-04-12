c0c89b6b9017e0cedb5a4f0db4efd2bb19c7ec57
AromaticArea3836 <150521365+AromaticArea3836@users.noreply.github.com>
2025-04-12 20:31:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import gifos
 
 t = gifos.Terminal(width=320, height=240, xpad=5, ypad=5)
 t.gen_text(text="Hello World!", row_num=1)
-t.gen_text(text="Hello VizXtreme and ukriu!", row_num=2)
+t.gen_text(text="Hello, VizXtreme and ukriu!", row_num=2)
 github_stats = gifos.utils.fetch_github_stats(
-    user_name="rickastleey"
+    user_name="xnggypr"
 )  # needs GITHUB_TOKEN in .env or as environment variable
 t.delete_row(row_num=1)
-t.gen_text(text=f"GitHub Name: {github_stats.user_name}", row_num=1, contin=True)
+t.gen_text(text=f"GitHub Name: {github_stats.account_name}", row_num=1, contin=True)
 t.gen_gif()
 image = gifos.utils.upload_imgbb(
     file_name="output.gif", expiration=60
