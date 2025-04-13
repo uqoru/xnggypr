68bd4896b2dd90c10e6c8ddcf1e79eea22656fa2
AromaticArea3836 <150521365+AromaticArea3836@users.noreply.github.com>
2025-04-13 07:57:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -50,27 +50,28 @@ boot_line("Hardware detection", 3, delay=2)
 print_dots_done(3)
 
 boot_line("Hardware detected:", 4)
-boot_line("  CPU: js.core/V8 Compatible rendering engine", 5, delay=2)
+boot_line("  CPU: VizXtreme a7, 1.2Ghz", 5, delay=2)
 boot_line("  RAM: 256MiB", 6, delay=2)
-boot_line("  Display: 800x600 16-bit color GitHub Markdown Renderer", 7, delay=2)
+boot_line("  GPU: they/them yahocu l3s", 7, delay=2)
+boot_line("  Display: 800x600 16-bit color ukriu renderer", 8, delay=2)
 t.clone_frame(15)
 
-boot_line("Beginning memory test...", 9, delay=4)
+boot_line("Beginning memory test...", 10, delay=4)
 
 # Memory test simulation
 for i in range(0, 262144, 8192):  # 256MiB Memory
-    t.delete_row(9)
-    t.gen_text(f"Beginning memory test... {i}KiB", 9)
+    t.delete_row(10)
+    t.gen_text(f"Beginning memory test... {i}KiB", 10)
 
-t.delete_row(9)
-boot_line(f"Beginning memory test...", 9)
-boot_line(" 262144KiB OK", 9, color="green", contin=True)
+t.delete_row(10)
+boot_line(f"Beginning memory test...", 10)
+boot_line(" 262144KiB OK", 10, color="green", contin=True)
 t.clone_frame(15)
 
 t.clear_frame()
 
 # Simulate the kernel boot process
-boot_line("lemmons ltd kernel.js initializing", 1)
+boot_line("rickastleeys megatrends(C), initializing", 1)
 print_dots_done(1)
 init_line("Starting system...", 2)
 init_line("Initializing hardware...", 3)
@@ -78,26 +79,26 @@ init_line("Checking filesystems...", 4)
 init_line("Mounting volumes...", 5)
 init_line("Starting networking service...", 6)
 init_line("Initializing display...", 7)
-t.gen_text("Welcome to lew's profile!", 9)
+t.gen_text("Welcome to rickastleey's profile!", 9)
 t.clone_frame(15)
 t.clear_frame()
 
 # Simulate login prompt
 t.set_font("CascadiaCodePL.ttf", 15)
 t.gen_text(text="github login: ", row_num=1)
-t.gen_typing_text(text="lewisdoesstuff", row_num=1, col_num=14, contin=True, speed=0.1)
+t.gen_typing_text(text="xnggypr", row_num=1, col_num=14, contin=True, speed=0.1)
 t.clone_frame(10)
 t.gen_text(text="Password: ", row_num=2)
 t.gen_typing_text(text="********", row_num=2, col_num=10, contin=True, speed=0.1)
 t.clone_frame(15)
 
 t.clear_frame()
-t.gen_text(text="Welcome, lewisdoesstuff! Last Login: 01-01-1970T00:00:01", row_num=1)
-t.set_prompt("\x1b[35mlewisdoesstuff\x1b[39m@\x1b[32mgithub\x1b[39m:~$ ")
+t.gen_text(text="Welcome, rickastleey! Last Login: 01-01-1999T00:00:19", row_num=1)
+t.set_prompt("\x1b[35xnggypr\x1b[39m@\x1b[32mgithub\x1b[39m:~$ ")
 t.gen_prompt(2)
-t.gen_typing_text(text="ghfetch -u lewisdoesstuff", row_num=2, contin=True, speed=0.1)
+t.gen_typing_text(text="ghfetch -u xnggypr", row_num=2, contin=True, speed=0.1)
 
-gh_stats = gifos.utils.fetch_github_stats("lewisdoesstuff")
+gh_stats = gifos.utils.fetch_github_stats("xnggypr")
 details_lines = f"""\x1b[96mUser Rating: \x1b[93m{gh_stats.user_rank.level}\x1b[0m
 \x1b[96mTotal Stars Earned: \x1b[93m{gh_stats.total_stargazers}\x1b[0m
 \x1b[96mTotal Commits (2023): \x1b[93m{gh_stats.total_commits_last_year}\x1b[0m
